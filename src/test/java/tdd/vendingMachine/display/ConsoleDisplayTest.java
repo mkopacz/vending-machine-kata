e8f3f174f7b4e4ddf8651b4d797fb695c0273d02
Mariusz Kopacz <mariusz.kopacz90@gmail.com>
2017-03-12 13:11:05 +0100

    refactor display tests

--- src/test/java/tdd/vendingMachine/display/ConsoleDisplayTest.java
+++ src/test/java/tdd/vendingMachine/display/ConsoleDisplayTest.java
@@ -10,30 +10,31 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class ConsoleDisplayTest {
 
+    private Display consoleDisplay;
     private ByteArrayOutputStream consoleOutput;
 
     @Before
     public void setUp() {
+        consoleDisplay = new ConsoleDisplay();
         consoleOutput = new ByteArrayOutputStream();
+
         System.setOut(new PrintStream(consoleOutput));
     }
 
     @Test
     public void shouldDisplayMessage() {
-        Display display = new ConsoleDisplay();
         String message = "example message";
 
-        display.displayMessage(message);
+        consoleDisplay.displayMessage(message);
 
         assertThat(consoleOutput.toString()).isEqualTo(message + "\n");
     }
 
     @Test
     public void shouldDisplayWarning() {
-        Display display = new ConsoleDisplay();
         String warning = "example warning";
 
-        display.displayWarning(warning);
+        consoleDisplay.displayWarning(warning);
 
         assertThat(consoleOutput.toString()).isEqualTo("WARNING! " + warning + "\n");
     }
